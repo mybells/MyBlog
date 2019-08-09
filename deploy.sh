b31dfc859a28c7b20af6d0af93048a021698f481
mybells <435541908@qq.com>
2019-08-09 14:27:25 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
-### 
-# @Author: 高建鹏
- # @Description: 
- # @Position: 
- # @Date: 2019-08-06 15:45:45
- # @LastEditors: 高建鹏
- ###
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run build:docs 
+npm run build:docs
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
