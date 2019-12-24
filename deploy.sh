7d0818fcd403dc2b8731fe9bbe951518740a34c2
mybells <435541908@qq.com>
2019-12-24 15:58:26 +0800

    d

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build:docs
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,11 +12,11 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init 
+git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
+# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名
 git push -f git@github.com:mybells/mybells.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
